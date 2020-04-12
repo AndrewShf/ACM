5a43a2747fecba63191471c2bf0c1d13366cd890
石海丰 <shihaifeng1998@gmail.com>
2020-04-12 18:29:22 +0800

    LineSweep+Segmenttree ==> square count

---
+++ geometry/line_sweep_segment_tree.cpp
@@ -0,0 +1,103 @@
+
+/*
+http://acm.hdu.edu.cn/showproblem.php?pid=1542
+Line sweep + Segment tree
+Reference: https://oi-wiki.org/geometry/scanning/
+Maintain the Y-value information by using segment tree
+*/
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+
+#define ll long long
+#define ull unsigned long long
+
+using namespace std;
+
+const int MOD = 1e9+7;
+const int INF = 0x3f3f3f3f;
+const int PI = acos(-1.0);
+
+const int N = 3e2+5;
+
+int n;
+struct Tree {
+    double l, r;
+    double sum;
+    int inv;
+} tree[N<<3];
+
+struct Node {
+    int flag;
+    double x, y1, y2;
+} node[N<<3];
+
+double yval[N<<3];
+
+bool cmp (Node n1, Node n2) {
+    return n1.x < n2.x;
+}
+
+void build(int o, int l, int r) {  
+    tree[o].inv = 0;
+    tree[o].l = yval[l];
+    tree[o].r = yval[r];
+    tree[o].sum = 0;
+    if (r-l == 1)
+        return;
+    int m = (l+r)>>1;
+    build(o<<1, l, m); build(o<<1|1, m, r);
+}
+
+void pushup(int o) {
+    if (tree[o].inv > 0) {
+        tree[o].sum = tree[o].r-tree[o].l; // 区间有线段，算一次
+    } else {
+        tree[o].sum = tree[o<<1].sum+tree[o<<1|1].sum; // 区间无线段，计算子区间的和
+    }
+}
+
+void add(int o, double l, double r, int c) { // 点修改
+    if (l==tree[o].l && r==tree[o].r) {
+        tree[o].inv += c; // 加入线段次数
+        pushup(o); 
+        return;
+    }
+    if (l < tree[o<<1].r) add(o<<1, l, min(tree[o<<1].r, r), c); // 区间add
+    if (r > tree[o<<1|1].l) add(o<<1|1, max(l, tree[o<<1|1].l), r, c); // 区间add
+    pushup(o);
+}
+
+int main() {
+    int temp = 1;
+    while ((scanf("%d", &n)!=EOF) && n) {
+        double x1, x2, y1, y2;
+        for (int i = 0; i < n; ++i) {
+            scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+            node[i].x = x1; node[i].y1 = y1; node[i].y2 = y2; node[i].flag = 1;
+            node[i+n].flag = -1; node[i+n].x = x2; node[i+n].y1 = y1; node[i+n].y2 = y2;
+            yval[i+1] = y1;
+            yval[i+n+1] = y2;
+        }
+        sort(yval+1, yval+1+2*n); // 用来建树
+        sort(node, node+2*n, cmp);
+        build(1, 1, 2*n);
+        add(1, node[0].y1, node[0].y2, node[0].flag);
+        double ans = 0;
+        for (int j = 1; j < 2*n; ++j) {
+            ans += (node[j].x-node[j-1].x) * tree[1].sum;
+            add(1, node[j].y1, node[j].y2, node[j].flag);
+        }
+        printf("Test case #%d\nTotal explored area: %.2lf\n\n", temp++, ans);
+    }
+	return 0;
+}
